571b936938cc570e3919922ac1b5e1db2d6ad55e
Wiryono Lauw <wiryonolau@gmail.com>
2025-01-04 18:05:41 +0700

    Fix nullable not set to true when default empty

--- src/Metadata/Hydrator/ColumnObjectHydrator.php
+++ src/Metadata/Hydrator/ColumnObjectHydrator.php
@@ -6,4 +6,19 @@ use Laminas\Hydrator\ClassMethodsHydrator;
 
 class ColumnObjectHydrator extends ClassMethodsHydrator
 {
+    public function hydrate(array $data, object $object)
+    {
+        // When column default is empty string is nullable must be set to true
+        // Integer primary key doesn't have is_nullable key skip if not set 
+        if (
+            isset($data["is_nullable"])
+            and !$data["is_nullable"]
+            and empty($data["column_default"])
+        ) {
+            $data["is_nullable"] = true;
+            $data["column_default"] = null;
+        }
+
+        return parent::hydrate($data, $object);
+    }
 }
